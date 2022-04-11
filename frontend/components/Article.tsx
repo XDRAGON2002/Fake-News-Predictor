8e2b642c87770e284b6c061a00254069cca1dcd6
gaurav879 <gaurav91306@gmail.com>
2022-04-11 23:59:36 +0530

    added material design to news page

--- frontend/components/Article.tsx
+++ frontend/components/Article.tsx
@@ -1,22 +1,33 @@
 import Link from "next/link";
 import styles from '../styles/Home.module.css';
+import { Typography, Button } from '@mui/material';
 
 const Article = ({ data }) => {
-    console.log(data["url"]);
+    const title = String(data["title"]).split("-")
+    console.log(title);
     return (
-        <div className={styles.article}>
-            {/* <span>{data["source"]["name"]}</span> */}
-            <br />
-            <div className={styles.articlelist}>
-                <Link href={data["url"]}>
-                    <a>
-                        <span className={styles.articletitle}>{data["title"]}</span>
-                    </a>
-                </Link>
-            </div>
+        <div className={styles.cont}>
+            <div className={styles.article}>
+                <div>
+                <br />
+                <div >
+                    <Typography className={styles.articletitle}>{data["title"]}</Typography>
+                    <Link href={data["url"]}><Button className={styles.articlebutton} variant="contained">Read More...</Button></Link>
+                    
+                </div>
 
-            <br />
-            <br />
+                <br />
+                <br /> 
+                </div>
+                {/* <span>{data["source"]["name"]}</span> */}
+                <div className={styles.rhs}>
+                    <div className={styles.authenticity}>Authenticity of news</div>
+                    
+                
+                <Button variant="contained" className={styles.authenticitybutton}>{"val"}</Button>
+                </div>
+            </div>
+            
         </div>
     );
 };
