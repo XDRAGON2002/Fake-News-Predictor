3f4c074806d80284636ebdacf4e0988ef3564b1f
pratul20 <sharma.pratul20@gmail.com>
2022-04-19 00:43:48 +0530

    Completed frontend designing

--- frontend/components/Article.tsx
+++ frontend/components/Article.tsx
@@ -1,33 +1,82 @@
 import Link from "next/link";
 import styles from '../styles/Home.module.css';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import { Typography, Button } from '@mui/material';
 import headlineFormatter from '../utils/headlineFormat';
 const Article = ({ data }) => {
+
+    const [prob, setprob] = useState(0)
+    // const [result, setresult] = useState("0 %")
+
     const title = headlineFormatter(String(data["title"]))
+    let result:number = 0;
+
+    const func = () => {
+        axios.post('http://127.0.0.1:5000/predict', {
+            news_headline: title.headline,
+        })
+            .then((response) => {
+                console.log(response);
+                result = response.data.result
+                if (result == 0) {
+                    setprob(100 - response.data.prob)
+                }
+                else {
+                    setprob(response.data.prob)
+                }
+                // console.log(prob)
+            }, (error) => {
+                console.log(error);
+            });
+    }
+
+
+
+    // useEffect( () => {
+    //     (async () => {
+    //         await axios.post('http://127.0.0.1:5000/predict', {
+    //             news_headline: title.headline,
+    //         })
+    //         .then((response) => {
+    //             console.log(response);
+    //             setprob(response.data.prob)
+    //             console.log(prob)
+    //         }, (error) => {
+    //             console.log(error);
+    //         });
+    //       setprob(prob);
+    //       setresult(String(prob*100)+" %");
+    //     })
+    // },[])
+
+
     return (
         <div className={styles.cont}>
             <div className={styles.article}>
                 <div>
-                <br />
-                <div >
-                    <Typography gutterBottom variant="h5" className={styles.articletitle}>{title.headline}</Typography>
-                    <Typography className={styles.articletitle}>{title.author}</Typography>
-                    <a href={data["url"]}><Button className={styles.articlebutton} variant="contained">Read More...</Button></a>
-                    
-                </div>
+                    <br />
+                    <div >
+                        <Typography gutterBottom variant="h5" className={styles.articletitle}>{title.headline}</Typography>
+                        <Typography className={styles.articletitle}>{title.author}</Typography>
+                        <a href={data["url"]}><Button className={styles.articlebutton} variant="contained">Read More...</Button></a>
 
-                <br />
-                <br /> 
+                    </div>
+
+                    <br />
+                    <br />
                 </div>
                 {/* <span>{data["source"]["name"]}</span> */}
                 <div className={styles.rhs}>
                     <div className={styles.authenticity}>Authenticity of news</div>
-                    
-                
-                <Button variant="contained" className={styles.authenticitybutton}>{"val"}</Button>
+
+                    {
+                        prob == 0 ? <div><Button variant="contained" className={styles.authenticitybutton} onClick={func}>Predict</Button></div> : <Button variant="contained" className={styles.authenticitybutton}>{prob.toPrecision(4) * 100} %</Button>
+                    }
+
                 </div>
             </div>
-            
+
         </div>
     );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,25 +1,68 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import styles from '../styles/Home.module.css'
 import NavDrawer from '../components/NavDrawer';
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+import greentick from "../public/greentick.png";
 
 const Home: NextPage = () => {
 
-  const [news,setNews] = useState("");
+  const [news, setNews] = useState("");
+  const [openAlert, setOpenAlert] = useState(false);
+  const [prediction, setPrediction] = useState(false)
+  const [val,setVal] = useState(0)
+  const [prob,setProb] = useState(0.01)
+  let result: number = 0;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpenAlert(false)
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [openAlert]);
 
 
-  const func = () => {
-    axios.post('http://127.0.0.1:5000/predict', {
+  const func = async () => {
+    let response = await axios.post('http://127.0.0.1:5000/predict', {
       news_headline: news,
     })
-    .then((response) => {
-      console.log(response);
-    }, (error) => {
-      console.log(error);
-    });
+
+    console.log(response);
+    result = response.data.data
+    if (result === 0) {
+      setProb(1 - response.data.prob)
+    }
+    else {
+      setProb(response.data.prob)
+    }
+    setProb(prob * 100)
+    console.log(prob)
+    let updatedprob = result===0 ? (1-response.data.prob)*100 : (response.data.prob*100);
+    console.log("updated prob: "+updatedprob) 
+    setProb(updatedprob)
+    if (updatedprob > 70) {
+      setVal(1);
+    }
+    else if (updatedprob > 40) {
+      setVal(2);
+    }
+    else {
+      setVal(3);
+    }
+
+    console.log(val)
+    setPrediction(true)
+  }
+
+  const alert = () => {
+    setOpenAlert(true)
   }
 
   const handleTextChange = (e) => {
@@ -28,7 +71,7 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <NavDrawer></NavDrawer>
+      <NavDrawer />
 
       <main className={styles.main}>
 
@@ -37,12 +80,42 @@ const Home: NextPage = () => {
             <form className={styles.inputscreen}>
               <TextField id="outlined-multiline-static" onChange={handleTextChange} className={styles.textfield} label="Enter your News here" multiline rows={7} placeholder="Enter your News here" />
               <p className={styles.p}>OR</p>
-              <Button variant="contained" component="label" className={styles.button}>Upload<input type="file" accept="audio/mp3" hidden/> </Button>
+              <Button variant="contained" component="label" className={styles.button} onClick={alert}>Upload</Button>
             </form>
           </div>
           <br></br>
-          <Button variant="contained" className={styles.pbutton} onClick={func}>Predict</Button>
+          <div><Button variant="contained" className={styles.pbutton} onClick={func}>Predict</Button></div>
+          {
+            val === 1 && <div className={styles.predictionBox}><img src="greentick.png" width={50} height={50} /> <p>&nbsp;&nbsp;The Authenticity of the news is {prob.toPrecision(4)} %</p> </div>
+          }
+          {
+            val === 2 && <div className={styles.predictionBox}><img src="exclaimation.png" width={50} height={50} /> <p>&nbsp;&nbsp;The Authenticity of the news is {prob.toPrecision(4)} %</p></div>
+          }
+          {
+            val === 3 && <div className={styles.predictionBox}><img src="wrong.png" width={50} height={50} /> <p>&nbsp;&nbsp;The Authenticity of the news is {prob.toPrecision(4)} %</p></div>
+          }
         </div>
+
+        <Collapse in={openAlert}>
+          <Alert
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpenAlert(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+            sx={{ mb: 2 }}
+            severity="info"
+          >
+            This feature is under development
+          </Alert>
+        </Collapse>
 
       </main>
 
