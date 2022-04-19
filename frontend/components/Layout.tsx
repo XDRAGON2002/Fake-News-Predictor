640a1b479e5738ef6d8d7dbac559707c1a888010
gaurav879 <gaurav91306@gmail.com>
2022-04-19 15:25:32 +0530

    Converted website to a PWA

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,10 +1,19 @@
 import NavDrawer from "./NavDrawer";
-
+import Head from "next/head";
 const Layout = () => {
     return (
-        <div data-testid="layout">
-            <NavDrawer/>
-        </div>
+        <>
+            <Head>
+                <title>
+                    Fake News Predictor
+                </title>
+                <link rel="manifest" href="/manifest.json" />                 
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <div data-testid="layout">
+                <NavDrawer/>
+            </div>
+        </>
     );
 };
 
